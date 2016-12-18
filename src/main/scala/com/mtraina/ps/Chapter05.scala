7878fb0576728c62fe20fea3f4de764af0f1f41c
mtraina <matteo.traina.mail@gmail.com>
2016-12-18 15:59:35 +0100

    string interpolation and object equality

--- src/main/scala/com/mtraina/ps/Chapter05.scala
+++ src/main/scala/com/mtraina/ps/Chapter05.scala
@@ -12,4 +12,22 @@ object Chapter05 {
 
   // add formatting instructions
   println(f"${Math.PI}%.5f") // prints 3.14159
+
+  /**
+   * Operators are methods
+   */
+   val sum = 1 + 2 // in this case Scala invokes the method + on the Int 1, so 1.+(2)
+
+  /**
+    * Objects equality
+    * Scala compares values not references like in Java
+    */
+  1 == 2                      // false
+  2 == 2                      // true
+  null == List(1,2,3)         // false
+  List(1,2,3) == List(1,2,3)  // true
+  "he" + "llo" == "hello"     // true
+
+
+
 }
