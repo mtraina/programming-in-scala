0eb0f9c0c033dcec29644248b23f91c469ff808b
mtraina <matteo.traina.mail@gmail.com>
2017-01-03 20:23:12 +0100

    file matcher refactored

--- src/main/scala/com/mtraina/ps/Chapter09.scala
+++ src/main/scala/com/mtraina/ps/Chapter09.scala
@@ -19,8 +19,42 @@ object Chapter09 {
         yield file
 
     //returns all the files matching a specific regex in their name
-    def filesregex(query: String) =
+    def filesRegex(query: String) =
       for(file <- filesHere; if file.getName.matches(query))
         yield file
+  }
+
+  /**
+    * Refactored file matcher where we reuse the common part of matching the file name
+    */
+  object FileMatcherRefactored {
+    private def filesHere = new java.io.File(".").listFiles()
+
+    def filesMatching(query: String, matcher: (String, String) => Boolean) = {
+      for(file <- filesHere; if matcher(file.getName, query))
+        yield file
+    }
+
+    // the first placeholder (_) will be fulfilled with the value of "file.getName" being it the first parameter passed
+    // to the function matcher inside the method fileMatching
+    // similarly the second placeholder will get the value of "query", being it the second parameter passed
+    def fileEnding(query: String) = filesMatching(query, _.endsWith(_))
+    def fileContaining(query: String) = filesMatching(query, _.contains(_))
+    def fileRegex(query: String) = filesMatching(query, _.matches(_))
+  }
+
+  /**
+    * Refactored file matcher using closures: query is the free variable
+    */
+  object FileMatcherClosure {
+    private def filesHere = new java.io.File(".").listFiles()
+
+    private def filesMatching(matcher: String => Boolean) =
+      for(file <- filesHere; if matcher(file.getName))
+        yield file
+
+    def fileEnding(query: String) = filesMatching(_.endsWith(query))
+    def fileContaining(query: String) = filesMatching(_.contains(query))
+    def fileRegex(query: String) = filesMatching(_.matches(query))
   }
 }
