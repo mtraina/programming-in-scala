627420aec925bcef1ff47c80d4983b053f93041a
mtraina <matteo.traina.mail@gmail.com>
2017-01-29 08:31:31 +0100

    composition and inheritance I

---
+++ src/main/scala/com/mtraina/ps/Chapter10.scala
@@ -0,0 +1,13 @@
+package com.mtraina.ps
+
+object Chapter10 {
+
+  /**
+    * An abstract class contains method that may not be implemented, so it can't be instantiated
+    */
+  abstract class Element {
+    def contents: Array[String]
+  }
+
+
+}
