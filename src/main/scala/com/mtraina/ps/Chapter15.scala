cbdd08124508304fb14b3c60fb41d72f8ed9a7e3
mtraina <matteo.traina.mail@gmail.com>
2017-04-09 10:54:46 +0200

    chapter 15: pattern matching intro

--- src/main/scala/com/mtraina/ps/Chapter15.scala
+++ src/main/scala/com/mtraina/ps/Chapter15.scala
@@ -7,8 +7,11 @@ object Chapter15 {
   case class UnOp(operator: String, arg: Expr) extends Expr
   case class BinOp(operator: String, left: Expr, right: Expr) extends Expr
 
-  // when the "case" keyword is used, the compiler adds some syntactic parts
-  // it adds a factory to create objects without specifying the keyword "new"
+  /**
+    * Case classes
+    * when the "case" keyword is used, the compiler adds some syntactic parts
+    * it adds a factory to create objects without specifying the keyword "new"
+    */
   val v = Var("v")
 
   // it is particularly useful when the creation of objects are nested
@@ -21,4 +24,18 @@ object Chapter15 {
 
   // lastly the compiler creates a copy method useful for making copies of objects with small changes on them
   op.copy(operator = "-") // res5: BinOp = BinOp(-,Number(1.0),Var(v))
+
+  /**
+    * Pattern matching
+    * it follows the rule "selector match {alternatives}"
+    * match is an expression in Scala, so it does return a value
+    * if none of the expressions match, an exception in thrown
+    * the wildcard "_" is the default value
+    */
+  def simplifyTop(expr: Expr): Expr = expr match {
+    case UnOp("-", UnOp("-", e)) => e   // double negation
+    case BinOp("+", e, Number(0)) => e  // adding zero
+    case BinOp("*", e, Number(1)) => e  // multiply by one
+    case _ => expr
+  }
 }
