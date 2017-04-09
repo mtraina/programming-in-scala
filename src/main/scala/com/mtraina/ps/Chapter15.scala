4386132ef17302402cf2e22015ef20f75e2c13d5
mtraina <matteo.traina.mail@gmail.com>
2017-04-09 10:37:44 +0200

    chapter 15: case classes convenience methods

--- src/main/scala/com/mtraina/ps/Chapter15.scala
+++ src/main/scala/com/mtraina/ps/Chapter15.scala
@@ -13,4 +13,12 @@ object Chapter15 {
 
   // it is particularly useful when the creation of objects are nested
   val op = BinOp("+", Number(1), Var("v"))
+
+  // the compiler adds also a default implementation of the methods equals, hashCode and toString
+  v.toString // res3: String = Var(v)
+  v.hashCode // res2: Int = 717264670
+  v.equals(Var("v")) // res4: Boolean = true
+
+  // lastly the compiler creates a copy method useful for making copies of objects with small changes on them
+  op.copy(operator = "-") // res5: BinOp = BinOp(-,Number(1.0),Var(v))
 }
